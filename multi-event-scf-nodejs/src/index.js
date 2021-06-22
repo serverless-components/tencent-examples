5f82f8d2fc6b80b0f361741cae98c54482171590
yugasun <yuga_sun@163.com>
2021-06-22 20:12:38 +0800

    fix: update api

--- multi-event-scf-nodejs/src/index.js
+++ multi-event-scf-nodejs/src/index.js
@@ -1,23 +1,32 @@
 'use strict';
 exports.index = async (event, context) => {
-    return {
-      msg: 'Hello Serverless',
-    };
+  return {
+    message: 'Tencent SCF execute successful!',
+    input: event,
   };
-  
-  exports.list = async (event, context) => {
-    return [
-      {
-        id: 1,
-        name: 'test',
-      },
-    ];
+};
+
+exports.hello = async (event, context) => {
+  const name = event.pathParameters.name
+  return {
+    message: `Hello from ${name || 'Anonymous'}`,
+    body: event.body || null,
+    queries: event.queryString || null,
   };
-  
-  exports.detail = async (event, context) => {
-    return {
+};
+
+exports.list = async (event, context) => {
+  return [
+    {
       id: 1,
       name: 'test',
-    };
+    },
+  ];
+};
+
+exports.detail = async (event, context) => {
+  return {
+    id: 1,
+    name: 'test',
   };
-  
+};
