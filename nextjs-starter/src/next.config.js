3030d9fc15f2d97a2968c6eef79c76a73fca158f
Avril Li <linxforever@vip.qq.com>
2021-07-15 14:47:52 +0800

    chore: upgrade nextjs-starter (#80)

---
+++ nextjs-starter/src/next.config.js
@@ -0,0 +1,12 @@
+const isProd = process.env.NODE_ENV === 'production'
+
+module.exports = {
+  env: {
+    STATIC_URL: isProd ? process.env.STATIC_URL : ''
+  },
+  assetPrefix: isProd ? process.env.STATIC_URL : '',
+  reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true
+  }
+}
