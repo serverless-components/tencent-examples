0f5a1d2fab108c861371838356ee5d7f338157d6
Jominja <1075495554@qq.com>
2021-09-27 11:27:27 +0800

    web-function

--- nextjs-starter/src/pages/index.js
+++ nextjs-starter/src/pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/index.module.css'
 
-export default function Home() {
+export default function Index() {
   return (
     <div className={styles.container}>
       <Head>
